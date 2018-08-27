6e1d16300ad452964a6c8d4518a8a0b7bea249cc
I335191 <alex.dalentzas@sap.com>
2018-08-27 11:05:53 +0300

    Turn it to .py

--- Export+Table+From+PDF.py
+++ Export+Table+From+PDF.py
@@ -1,25 +1,11 @@
-
-# coding: utf-8
-
-# In[1]:
-
+#import tabula_py not tabula
 from tabula import read_pdf
-import pyhdb
-import pandas as pd
 
-
-# In[8]:
-
-path_to_pdf = r"C:\Users\I335191\dev\python_samples\data\Sample_employees_data.pdf"
+path_to_pdf = r"<Path>\data\Sample_employees_data.pdf"
 df = read_pdf(path_to_pdf ,pages=1,guess=False)
-
-
-# In[6]:
 
 df
 
-
-# In[ ]:
 
 
 
